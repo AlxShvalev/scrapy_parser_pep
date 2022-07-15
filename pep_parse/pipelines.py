75df7c0c40ef57c26f984d7652ae9f7bbeaa9c2d
AlxShvalev <shvalevalx@gmail.com>
2022-07-15 19:29:12 +0500

    fix flake8 errors

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -23,19 +23,15 @@ class PepParsePipeline:
     def close_spider(self, spider):
         self.status_summary['total'] = sum(self.status_summary.values())
         self.write_to_csv(
-            filename=FILE_NAME,
+            name=FILE_NAME,
             dict_to_save=self.status_summary,
             header=('Статус', 'Количество')
         )
 
-    def write_to_csv(self, filename: str, dict_to_save: dict, header: tuple):
+    def write_to_csv(self, name: str, dict_to_save: dict, header: tuple):
         """Записывает файл filename_datetime_format.csv в BASE_DIR"""
-        file = (
-            filename
-            + '_'
-            + str(datetime.now().strftime(DATE_FORMAT))
-            + '.csv'
-        )
+        file = name + '_' + str(datetime.now().strftime(DATE_FORMAT)) + '.csv'
+
         full_file_name = os.path.join(BASE_DIR, file)
         with open(full_file_name, mode='w', encoding='utf-8') as f:
             writer = csv.writer(f)
